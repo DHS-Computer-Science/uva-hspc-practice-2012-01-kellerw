89983cfb7642bc46f248a248546719b38fd14796
Thor Keller <kellerw@users.noreply.github.com>
2016-03-16 08:50:30 -0400

    Testing if git works

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
-//TODO: <Name>
-//TODO: <Date>
+//Thor
+//Today
 //Practice 2012 - 01: Grading Exams
 
 public class Main {
   public static void main(String[] args) {
-
+    System.out.println("test");
     System.exit(0);
   }
 }
